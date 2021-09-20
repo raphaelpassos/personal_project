6a8835762a97fd86396164d26c03ea7f0a6a7e81
raphaelpassos <raphael_magalhaes@live.com>
2021-09-20 16:39:08 -0300

    implementation of scroll-arrow

---
+++ js/js.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	var winW = innerWidth;
+	var winH = innerHeight;
+	var intro = document.getElementById('intro');
+	var part1 = intro.getElementsByClassName('part1')[0];
+}
