57c733b497d20d0e3d02c05e47944c6320155ba0
raphaelpassos <raphael_magalhaes@live.com>
2021-09-22 20:40:16 -0300

    implementation slider show

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('.logo').slick({
+    centerMode: true,
+    centerPadding: '140px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  
