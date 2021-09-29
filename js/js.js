b43f74c222a39ce2b65fc75bfc73dbfcf2cb9b14
raphaelpassos <raphael_magalhaes@live.com>
2021-09-29 16:46:31 -0300

    correction of margin

--- js/js.js
+++ js/js.js
@@ -3,4 +3,5 @@ window.onload = function(){
 	var winH = innerHeight;
 	var intro = document.getElementById('intro');
 	var part1 = intro.getElementsByClassName('part1')[0];
+	part1.main.cssText = 'width:'+winW+'px;height:'+winH+'px';
 }
